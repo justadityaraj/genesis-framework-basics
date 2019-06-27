417ab4326bfe75a1450a544b010625f710f96955
Aditya Raj <45910873+justadityaraj@users.noreply.github.com>
2019-06-27 21:15:53 +0530

    1.9

--- remove-entry-title.php
+++ remove-entry-title.php
@@ -1,5 +1,5 @@
 <?php
 //* Do NOT include the opening php tag shown above. Copy the code shown below.
-//* Remove the entry title (requires HTML5 theme support)
 
+//* Remove the entry title (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
